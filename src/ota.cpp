950b5a3ff3bb8aead4f965f04465e392452315ce
Donghee Park <dongheepark@gmail.com>
2025-01-30 11:58:56 +0900

    Refactor Unpack functions

--- src/ota.cpp
+++ src/ota.cpp
@@ -57,3 +57,35 @@ void UnpackChannels4x10ToUInt11(OTA_Channels_4x10 const * const srcChannels4x10,
         bitsMerged -= srcBits;
     }
 }
+
+void PackUInt11ToChannels4x2(const crsf_channels_t* src, uint8_t* destChannels4x2, uint8_t isHighAux) {
+    // Pack the high channels (either CH5-CH8 or CH9-CH12 depending on isHighAux)
+    if (isHighAux) {
+        // Pack the high channel(11-bits) into a 2-bits value
+        *destChannels4x2 = (CRSF_to_N(src->ch9, 4) << 0) |
+                  (CRSF_to_N(src->ch10, 4) << 2) |
+                  (CRSF_to_N(src->ch11, 4) << 4) |
+                  (CRSF_to_N(src->ch12, 4) << 6);
+    } else {
+        *destChannels4x2 = (CRSF_to_N(src->ch5, 4) << 0) |
+                  (CRSF_to_N(src->ch6, 4) << 2) |
+                  (CRSF_to_N(src->ch7, 4) << 4) |
+                  (CRSF_to_N(src->ch8, 4) << 6);
+    }
+}
+
+void UnpackChannels4x2ToUInt11(uint8_t const srcChannels4x2, uint32_t * const dest, uint8_t isHighAux) {
+  if (isHighAux) {
+    dest[9] = N_to_CRSF((srcChannels4x2 >> 0) & 0x03, 3);
+    dest[10] = N_to_CRSF((srcChannels4x2 >> 2) & 0x03, 3);
+    dest[11] = N_to_CRSF((srcChannels4x2 >> 4) & 0x03, 3);
+    dest[12] = N_to_CRSF((srcChannels4x2 >> 6) & 0x03, 3);
+  } else {
+    dest[5] = N_to_CRSF((srcChannels4x2 >> 0) & 0x03, 3);
+    dest[6] = N_to_CRSF((srcChannels4x2 >> 2) & 0x03, 3);
+    dest[7] = N_to_CRSF((srcChannels4x2 >> 4) & 0x03, 3);
+    dest[8] = N_to_CRSF((srcChannels4x2 >> 6) & 0x03, 3);
+  }
+}
+
+
--- src/ota.h
+++ src/ota.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdint.h>
+#include "crsf_protocol.h"
 
 #define PACKED __attribute__((packed))
 
@@ -13,5 +14,16 @@ typedef struct {
   OTA_Channels_4x10 chHigh; // AUX2-5 or AUX6-9
 } PACKED RC_Channels_t;
 
+typedef struct {
+  uint8_t packetType: 2,
+          free: 4,
+          isHighAux: 1, // true if chHigh are AUX6-10
+          ch4: 1;   // AUX1, included up here so ch0 starts on a byte boundary
+  uint8_t raw[10];
+} PACKED RC_Channels_Encrypted_t;
+
 void PackUInt11ToChannels4x10(uint32_t const * const src, OTA_Channels_4x10 * const destChannels4x10);
 void UnpackChannels4x10ToUInt11(OTA_Channels_4x10 const * const srcChannels4x10, uint32_t * const dest);
+void PackUInt11ToChannels4x2(const crsf_channels_t* src, uint8_t* destChannels4x2, uint8_t isHighAux);
+void UnpackChannels4x2ToUInt11(uint8_t const srcChannels4x2, uint32_t * const dest, uint8_t isHighAux);
+ 
